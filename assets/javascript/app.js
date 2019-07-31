dc5ecc7292914acd836ee4b443c5c8c499798970
Matt Joseph <mattomjoe@yahoo.com>
2019-07-30 23:35:29 -0500

    move timer and buttons functions into global scope

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,69 +7,7 @@ $(document).ready(function () { // ALWAYS INCLUDE!!
         var timerStart = 10; // Sets timer to start at 10 seconds
         var intervalId; // Variable that will hold our interval ID when we execute the "run" function
 
-        function start() {
-
-                rightAnswerTotal = 0; // Var to hold right answer total at end of game
-                wrongAnswerTotal = 0; // Var to hold wrong answer total at end of game
-                unAnsweredTotal = 0; // Var to hold unanswered questions (timed out) at end of game
-                timerStart = 10; // Sets timer to start at 10 seconds
-                intervalId; // Variable that will hold our interval ID when we execute the "run" function
-
-        }
-
-        // Create array for trivia game that will hold questions, answer arrays, objects
-
-        var gameQuestions = [{
-                question1: "Minnesota is home to the U.S. Hockey Hall of Fame located in which city?",
-                choices1: ["St. Paul", "Eveleth", "Duluth", "Bemidji"],
-                correctAnswer1: "Eveleth"
-
-        },
-        {
-                question2: "Which former Minnesota Viking player went on to serve as a Justice on the Minnesota Supreme Court?",
-                choices2: ["Jim Marshall", "Chuck Foreman", "Alan Page", "Carl Eller"],
-                correctAnswer2: "Alan Page"
-        },
-        {
-                question3: "Where did the Timberwolves play in their first season?",
-                choices3: ["Metrodome", "Civic Center", "Met Center", "Williams Arena"],
-                correctAnswer3: "Metrodome"
-        },
-        {
-                question4: "How many championships have the Lynx won?",
-                choices4: ["0", "1", "3", "4"],
-                correctAnswer4: "4"
-        },
-        {
-                question5: "In the 1991 World Series between the Minnesota Twins and Atlanta Braves, how many games were decided by one run?",
-                choices5: ["2", "3", "4", "5"],
-                correctAnswer5: "5"
-        }];
-
-        console.log(gameQuestions[0].choices1[1]);
-
-        $("#start").click(function () {
-
-                // Add image for question to the page
-
-                //$("$question-picture").remove();
-                $("#question-picture").replaceWith("<img src='assets/images/question1.jpg'/>");
-
-                // Add the question to the page
-                $("#question").text(gameQuestions[0].question1);
-
-                // Add the buttons to the page
-
-                $("#button1").html("<button>" + gameQuestions[0].choices1[0] + "</button>");
-                $("#button2").html("<button>" + gameQuestions[0].choices1[1] + "</button>");
-                $("#button3").html("<button>" + gameQuestions[0].choices1[2] + "</button>");
-                $("#button4").html("<button>" + gameQuestions[0].choices1[3] + "</button>");
-
-                // Remove the start button from the page
-                $("#start").remove();
-
-
-                // *** TIMER ***
+        function timer(){ // *** TIMER ***
 
                 //  Set our number counter to 10.
                 var number = 11;
@@ -121,8 +59,78 @@ $(document).ready(function () { // ALWAYS INCLUDE!!
 
                 //  Execute the run function.
                 run();
+        }
+
+        function buttons () {
+                // Add the buttons to the page
+
+                $("#button1").html("<button>" + gameQuestions[0].choices1[0] + "</button>");
+                $("#button2").html("<button>" + gameQuestions[0].choices1[1] + "</button>");
+                $("#button3").html("<button>" + gameQuestions[0].choices1[2] + "</button>");
+                $("#button4").html("<button>" + gameQuestions[0].choices1[3] + "</button>");
+        }
+
+        function start() {
+
+                rightAnswerTotal = 0; // Var to hold right answer total at end of game
+                wrongAnswerTotal = 0; // Var to hold wrong answer total at end of game
+                unAnsweredTotal = 0; // Var to hold unanswered questions (timed out) at end of game
+                timerStart = 10; // Sets timer to start at 10 seconds
+                intervalId; // Variable that will hold our interval ID when we execute the "run" function
+
+        }
+
+        // Create array for trivia game that will hold questions, answer arrays, objects
+
+        var gameQuestions = [{
+                question1: "Minnesota is home to the U.S. Hockey Hall of Fame located in which city?",
+                choices1: ["St. Paul", "Eveleth", "Duluth", "Bemidji"],
+                correctAnswer1: "Eveleth"
+
+        },
+        {
+                question2: "Which former Minnesota Viking player went on to serve as a Justice on the Minnesota Supreme Court?",
+                choices2: ["Jim Marshall", "Chuck Foreman", "Alan Page", "Carl Eller"],
+                correctAnswer2: "Alan Page"
+        },
+        {
+                question3: "Where did the Timberwolves play in their first season?",
+                choices3: ["Metrodome", "Civic Center", "Met Center", "Williams Arena"],
+                correctAnswer3: "Metrodome"
+        },
+        {
+                question4: "How many championships have the Lynx won?",
+                choices4: ["0", "1", "3", "4"],
+                correctAnswer4: "4"
+        },
+        {
+                question5: "In the 1991 World Series between the Minnesota Twins and Atlanta Braves, how many games were decided by one run?",
+                choices5: ["2", "3", "4", "5"],
+                correctAnswer5: "5"
+        }];
+
+// ******************** CORE OF GAME PAGE ***************************//
+
+        $("#start").click(function () {
+
+                // Add image for question to the page
+
+                //$("$question-picture").remove();
+                $("#question-picture").replaceWith("<img src='assets/images/question1.jpg'/>");
+
+                // Add the question to the page
+                $("#question").text(gameQuestions[0].question1);
+
+                buttons(); // Function adds buttons for question choices to page
+
+                // Remove the start button from the page
+                $("#start").remove();
+
+                timer();
 
         });
+
+//*******************************************************************/
 
         // Create a function to run if the user answer matches the correct answer
 
@@ -155,9 +163,14 @@ $(document).ready(function () { // ALWAYS INCLUDE!!
 
 
         /*
-         $("#button1, #button2, #button3, #button4").click(function() {
+         $(".button1, #button2, #button3, #button4").click(function() {
                 if 
                         alert("Correct!");
+
+                questionNum++;
+                if (questionNum >= questions.length) {
+                        game over!
+                }
         });
         */
 
